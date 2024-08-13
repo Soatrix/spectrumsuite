bf4fed5ad358c8171f28cd2f24379c0233cbb3a3
Dan Alexander <admin@randamonium.co.nz>
2024-08-14 01:52:44 +1200

    Add Project Model

--- projectadmin/models.py
+++ projectadmin/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_projects')
+    team_members = models.ManyToManyField(User, related_name='projects')
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def add_team_member(self, user):
+        self.team_members.add(user)
+
+    def remove_team_member(self, user):
+        self.team_members.remove(user)
+
