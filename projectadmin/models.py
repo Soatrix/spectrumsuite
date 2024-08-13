c4c04db634dfcd697ca97481b698b93b05c54c41
Dan Alexander <admin@randamonium.co.nz>
2024-08-14 01:53:14 +1200

    Add Task Comment Model

--- projectadmin/models.py
+++ projectadmin/models.py
@@ -69,3 +69,12 @@ class Milestone(models.Model):
         self.save()
 
 
+class TaskComment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.created_at}"
+
