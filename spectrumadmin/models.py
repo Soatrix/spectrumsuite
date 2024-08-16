02693636d164905cc732bc80b81f2f2c947aaebe
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-16 23:31:11 +1200

    Add Notes to Services

--- spectrumadmin/models.py
+++ spectrumadmin/models.py
@@ -25,6 +25,7 @@ class Service(models.Model):
     contact_email = models.EmailField(blank=True)
     contact_phone_number = models.CharField(max_length=15, blank=True)
     active = models.BooleanField(default=True)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
