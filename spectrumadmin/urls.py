0eca5c35bec06f4b76810e29681b7492d1ed0574
FlopsiBunny <71574715+Fibbicles@users.noreply.github.com>
2024-08-16 23:08:26 +1200

    Add Support Service Detail View

--- spectrumadmin/urls.py
+++ spectrumadmin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("admin/services/", AdminServicesView.as_view(), name="spectrumadmin-services")
+    path("admin/services/", AdminServicesView.as_view(), name="spectrumadmin-services"),
+    path("admin/services/<int:id>/", AdminServiceDetailView.as_view(), name="spectrumadmin-service-detail")
 ]
--- spectrumadmin/views.py
+++ spectrumadmin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 from .models import *
 
@@ -16,4 +17,16 @@ class AdminServicesView(LoginRequiredMixin, TemplateView):
         context["user"] = self.request.user
         context["services"] = Service.objects.all()
         context["categories"] = ServiceCategory.objects.all()
+        return context
+
+class AdminServiceDetailView(LoginRequiredMixin, TemplateView):
+    template_name = "spectrumsuite/service-detail.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["MENU"] = settings.ADMIN_MENU
+        context["service"] = get_object_or_404(Service, id=self.kwargs["id"])
+        context["page_title"] = context["hoarde"].name
+        context["version"] = settings.VERSION
+        context["user"] = self.request.user
+
         return context
