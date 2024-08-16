c03b6854ea28062193445f48f59f0233192e3e9f
Dan Alexander <admin@randamonium.co.nz>
2024-08-16 13:01:35 +1200

    Start Spectrum Suite App

---
+++ spectrumadmin/menus.py
@@ -0,0 +1,12 @@
+ADMIN_MENU = [
+    {
+        "name": "Client Management",
+        "items": [
+            {
+                "icon": "users",
+                "label": "Clients",
+                "link": "projectadmin-projects"
+            }
+        ]
+    }
+]
